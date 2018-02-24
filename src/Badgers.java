9a722637014ce54011dcbf25353e21dc11dbfb54
JoshuaJasso1 <league@Mac-mini-4.local>
2018-02-24 11:26:14 -0800

    Finished My Age and started Badger

---
+++ src/Badgers.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Badgers {
+
+	public static void main(String[] args) {
+		String badger = "Badger, ";
+		String badger2 = "badger, ";
+		System.out.print(badger);
+		for (int i = 1; i < 10; i++) {
+			System.out.print(badger2);
+		}
+
+	}
+
+}
