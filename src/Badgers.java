580fa8c62fb1e6981a910eb14179d57ca2eef629
League <Level 2>
2018-03-03 11:24:51 -0800

    Finished Badger song and almost done with Tasty Tomato

--- src/Badgers.java
+++ src/Badgers.java
@@ -1,14 +1,73 @@
-import javax.swing.JOptionPane;
-
 public class Badgers {
 
 	public static void main(String[] args) {
 		String badger = "Badger, ";
 		String badger2 = "badger, ";
+		String badger4 = "badger";
 		System.out.print(badger);
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < 9; i++) {
 			System.out.print(badger2);
 		}
+		System.out.print(badger4);
+		System.out.println();
+		String mushroom = "Mushroom, ";
+		String mushroom2 = "mushroom, ";
+		System.out.print(mushroom);
+		System.out.print(mushroom2);
+		String a = "a ";
+		System.out.print(a);
+		System.out.println();
+		System.out.print(badger);
+		for (int i = 1; i < 9; i++) {
+			System.out.print(badger2);
+		}
+		System.out.print(badger4);
+		System.out.println();
+		System.out.print(mushroom);
+		System.out.print(mushroom2);
+		System.out.print(a);
+		System.out.println();
+		System.out.print(badger);
+		for (int i = 1; i < 9; i++) {
+			System.out.print(badger2);
+		}
+		System.out.print(badger4);
+		System.out.println();
+		String mush = "Mush-";
+		System.out.print(mush);
+		System.out.print(mushroom);
+		System.out.print(a);
+		System.out.println();
+		System.out.print(badger);
+		for (int i = 1; i < 9; i++) {
+			System.out.print(badger2);
+		}
+		System.out.print(badger4);
+		String african = "African! ";
+		String snake = "Snake, ";
+		String snake2 = "a snake";
+		String snake3 = "Snake! ";
+		String snake4 = "A snake, ";
+		String snake5 = "ooh it's a snake";
+		System.out.println();
+		System.out.print(african);
+		System.out.print(snake);
+		System.out.print(snake2);
+		System.out.println();
+		System.out.print(snake3);
+		System.out.print(snake4);
+		System.out.print(snake5);
+		String badger3 = "It's a badger, ";
+		System.out.println();
+		System.out.print(badger3);
+		for (int i = 1; i < 9; i++) {
+			System.out.print(badger2);
+		}
+		System.out.print(badger4);
+		System.out.println();
+		System.out.print(mushroom);
+		System.out.print(mushroom2);
+		System.out.print(a);
 
 	}
 
