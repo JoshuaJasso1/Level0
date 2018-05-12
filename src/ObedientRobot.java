df65739e250a7c618982bec574bb9e6d4d9bdf3f
JoshuaJasso1 <league@Mac-mini-3.local>
2018-05-12 11:20:32 -0700

    Finished Ninety nine bottles and I finished obedient robot

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -7,26 +7,32 @@ public class ObedientRobot {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String color = JOptionPane.showInputDialog("What color shape would you like, red, green, blue?");
+		if (color.toLowerCase().equals("red")) {
+			Josh.setPenColor(255,0,0);
+		}else if (color.toLowerCase().equals("blue")) {
+			Josh.setPenColor(0,0,255);
+		}else if (color.toLowerCase().equals("green")) {
+			Josh.setPenColor(0,255,0);
+		}else {
+			JOptionPane.showMessageDialog(null, "I don't have that color yet");
+		}
+		
 		String answer = JOptionPane
 				.showInputDialog("What shape would you like for me to draw, a square, a triangle, or a circle?");
-		Josh.setPenColor(255, 0, 0);
 		Josh.penDown();
 		Josh.setSpeed(1000);
 		if (answer.toLowerCase().equals("a square")) {
 
 			drawSquare();
-		} 
-		else if (answer.toLowerCase().equals("a triangle")) {
+		} else if (answer.toLowerCase().equals("a triangle")) {
 			drawTriangle();
-		}
-		else if (answer.toLowerCase().equals("a circle")){
+		} else if (answer.toLowerCase().equals("a circle")) {
 			drawCircle();
-		}
-		else{
+		} else {
 			JOptionPane.showMessageDialog(null, "Sorry I can't draw that yet");
 		}
 	}
-	
 
 	static void drawSquare() {
 		for (int i = 0; i < 4; i++) {
