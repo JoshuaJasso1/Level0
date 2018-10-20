a83a241dcffb7ce52f07e88d2eec4275031134ec
DemoliciousDt <league@Thes-Mac-mini.local>
2018-10-20 11:27:53 -0700

    Finished Fibonacci, finished the two test quizzes, finished the data tests, and need to do the last two questions in "What's the Output"

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,8 +3,15 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a = 1;
-		String b = 1;
+		int a = 0;
+		int b = 1;
+		System.out.print(a + ", " + b);
+		for (int i = 0; i <= 10; i++) {
+			int c = a + b;
+			a = b;
+			b = c;
+			System.out.print(", " + c);
+		}
 	}
 
 }
