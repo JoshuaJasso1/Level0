5569d6533586346f4ff861b2fe0c391549e705ef
angelvillarreal24 <league@Mac-mini-4.local>
2018-11-03 11:26:26 -0700

    I am almost done with for loop gauntlet, Just need to do number 7 and the bonus

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,28 @@
+
+public class ForLoopGauntlet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// for (int i = 2; i <= 100; i++) {
+		// if (i % 2 == 0); {
+		// System.out.println(i);
+	//	for (int i = 1; i <= 99; i++) {
+	//		if (i % 2 == 1) {
+//				System.out.println(i);
+		//	}
+		//}
+		//for(int i = 1; i<=500; i++) {
+		//	if(i % 2 ==0) {
+		//		System.out.println(i + " is even");
+		//	}
+		//	else {
+		//		System.out.println(i + " is odd");
+		//	}
+		//}
+		for(int i= 1; i<= 777; i ++) {
+			if (i % 7== 0){
+				System.out.println(i);
+			} 
+		}
+	}
+}
